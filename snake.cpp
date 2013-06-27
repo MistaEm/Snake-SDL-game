43b948c45b66d3418b61cadc46886388fc916efb
Tien Tran <ttran1155@gmail.com>
2013-06-27 16:50:40 -0700

    Create snake.cpp

---
+++ snake.cpp
@@ -0,0 +1,28 @@
+#include "SDL.h"
+#include "snake.h"
+using namespace std;
+
+snake::snake()
+:head(), tail()
+{}
+
+
+void push_back()
+{
+
+}
+
+void pop_back()
+{
+
+}
+
+void move_snake()
+{
+
+}
+
+void draw_snake()
+{
+
+}
